168607d362b51b64fbd94b9c14adc8271e1d3e21
leoesttevam <leonardoestevam17@gmail.com>
2023-09-11 14:31:56 -0300

    Usando JPQL

--- Spring-data/src/main/java/br/com/alura/Spring/data/repository/FuncionarioRepository.java
+++ Spring-data/src/main/java/br/com/alura/Spring/data/repository/FuncionarioRepository.java
@@ -1,7 +1,10 @@
 package br.com.alura.Spring.data.repository;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +14,7 @@ import br.com.alura.Spring.data.orm.Funcionario;
 public interface FuncionarioRepository extends CrudRepository<Funcionario, Integer>{
 
 	List<Funcionario> findByNome(String nome);
+	
+	@Query("SELECT f FROM Funcionario f WHERE f.nome = :nome AND f.salario >= :salario AND f.dataContratacao = :data")
+	List<Funcionario> findNomeSalarioMaiorDataContratacao(String nome, BigDecimal salario, LocalDate data);
 }
