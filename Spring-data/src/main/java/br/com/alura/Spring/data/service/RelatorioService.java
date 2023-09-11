5562deb8a1500c31da9b4136c2cc77c4b085b3b3
leoesttevam <leonardoestevam17@gmail.com>
2023-09-11 14:40:33 -0300

    Completando a aplicacao com JPQL

--- Spring-data/src/main/java/br/com/alura/Spring/data/service/RelatorioService.java
+++ Spring-data/src/main/java/br/com/alura/Spring/data/service/RelatorioService.java
@@ -1,5 +1,9 @@
 package br.com.alura.Spring.data.service;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Scanner;
 
@@ -15,6 +19,8 @@ public class RelatorioService {
 	
 	private final FuncionarioRepository repository;
 	
+	private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+	
 	public RelatorioService(FuncionarioRepository repository) {
 		this.repository = repository;
 	}
@@ -24,12 +30,15 @@ public class RelatorioService {
 			System.out.println("Qual ação de cargo deseja executar");
 			System.out.println("0 - Sair");
 			System.out.println("1 - Busca funcionario nome");
-			
+			System.out.println("2 - Buscar funcionario pelo nome, data contratação e salario");
 			int action = sc.nextInt();
 			
 			switch (action) {
 			case 1:
 				buscaFuncionarioNome(sc);
+				break;
+			case 2:
+				buscaFuncionarioNomeSalarioData(sc);
 				break;
 			default:
 				system = false;
@@ -45,5 +54,21 @@ public class RelatorioService {
 		
 		List<Funcionario> list = repository.findByNome(nome);
 		list.forEach(System.out::println);
+	}
+	
+	private void buscaFuncionarioNomeSalarioData(Scanner sc) {
+		System.out.println("Qual nome deseja pesquisar");
+		String nome = sc.next();
+		
+		System.out.println("Qual data de contratação deseja pesquisar");
+		String data = sc.next();
+		LocalDate time = LocalDate.parse(data, formatter);
+		
+		System.out.println("Qual salario deseja pesquisar");
+		BigDecimal salario = sc.nextBigDecimal();
+		
+		List<Funcionario> funcionarios = repository.findNomeSalarioMaiorDataContratacao(nome, salario, time);
+		
+		funcionarios.forEach(System.out::println);
 	}
 }
